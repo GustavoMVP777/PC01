05b6829403d521b6ad9aa2a2220214b657123e43
Gustavo <gustavo.oliva@etec.sp.gov.br>
2025-10-03 21:35:58 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+﻿double m, cm, km;
+
+Console.Write("Entre com a medida (em metros): ");
+m = Convert.ToDouble(Console.ReadLine());
+
+cm = m * 100;
+km = m / 1000;
+
+Console.WriteLine("\n--- Equivalência ---");
+Console.WriteLine($"{cm} cm");
+Console.WriteLine($"{m} m");
+Console.WriteLine($"{km} Km");
